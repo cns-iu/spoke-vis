44bf8e7149078e2d26cff6a7684d8ebb8f416084
axdanbol <daniel_bolin@yahoo.com>
2021-03-30 16:07:16 -0400

    Fixed testing + some linting rules

---
+++ website/projects/spoke/src/app/app.component.spec.ts
@@ -0,0 +1,17 @@
+import { Shallow } from 'shallow-render';
+
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+
+
+describe('AppComponent', () => {
+  let shallow: Shallow<AppComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(AppComponent, AppModule);
+  });
+
+  it('passes the dummy test', () => {
+    expect().nothing();
+  });
+});
