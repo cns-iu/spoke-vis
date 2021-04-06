9022dd8e8b386ec88a5f460617a772c182ee6784
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-04-06 12:27:17 -0400

    trying to get our data to map.

--- website/projects/spoke/src/app/shared/components/map/map.component.ts
+++ website/projects/spoke/src/app/shared/components/map/map.component.ts
@@ -8,7 +8,7 @@ import * as mapboxgl from 'mapbox-gl';
 })
 export class MapComponent implements OnInit {
   map!: mapboxgl.Map;
-  @Input() mapStyle: mapboxgl.Style = {
+  @Input() mapStyle: mapboxgl.Style | string = {
     'version': 8,
     'name': 'Blank',
     'center': [0, 0],
@@ -28,89 +28,23 @@ export class MapComponent implements OnInit {
   };
   @Input() dataSources!: mapboxgl.AnySourceData[];
 
-  parkSource: mapboxgl.AnySourceData = {
-    'type': 'geojson',
-    'data': {
-      'type': 'FeatureCollection',
-      'features': [
-        {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Polygon',
-            'coordinates': [
-              [
-                [-121.353637, 40.584978],
-                [-121.284551, 40.584758],
-                [-121.275349, 40.541646],
-                [-121.246768, 40.541017],
-                [-121.251343, 40.423383],
-                [-121.32687, 40.423768],
-                [-121.360619, 40.43479],
-                [-121.363694, 40.409124],
-                [-121.439713, 40.409197],
-                [-121.439711, 40.423791],
-                [-121.572133, 40.423548],
-                [-121.577415, 40.550766],
-                [-121.539486, 40.558107],
-                [-121.520284, 40.572459],
-                [-121.487219, 40.550822],
-                [-121.446951, 40.56319],
-                [-121.370644, 40.563267],
-                [-121.353637, 40.584978]
-              ]
-            ]
-          }
-        },
-        {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Point',
-            'coordinates': [-121.415061, 40.506229]
-          }
-        },
-        {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Point',
-            'coordinates': [-121.505184, 40.488084]
-          }
-        },
-        {
-          'type': 'Feature',
-          'geometry': {
-            'type': 'Point',
-            'coordinates': [-121.354465, 40.488737]
-          }
-        }
-      ]
-    }
-  } as unknown as mapboxgl.AnySourceData;
-
   ngOnInit() {
     this.loadMap();
   }
 
   loadMap(): void {
-    // this.map = new mapboxgl.Map({
-    //   container: 'map',
-    //   style: this.mapStyle,
-    //   center: [0, 0],
-    //   zoom: 0,
-    //   accessToken: 'pk.eyJ1IjoiYWRwaGlsbGlwczIwMTciLCJhIjoiY2s1NDNvaHdrMGZidDNobHFkdHB5MG1wcCJ9.NG8JyVzQuA6pP9UfZhlubg'
-    // });
-
     this.map = new mapboxgl.Map({
       container: 'map',
-      accessToken: 'pk.eyJ1IjoiYWRwaGlsbGlwczIwMTciLCJhIjoiY2s1NDNvaHdrMGZidDNobHFkdHB5MG1wcCJ9.NG8JyVzQuA6pP9UfZhlubg',
-      style: 'mapbox://styles/mapbox/outdoors-v11',
-      center: [-121.403732, 40.492392],
-      zoom: 10
+      style: this.mapStyle,
+      accessToken: 'pk.eyJ1IjoiYWRwaGlsbGlwczIwMTciLCJhIjoiY2s1NDNvaHdrMGZidDNobHFkdHB5MG1wcCJ9.NG8JyVzQuA6pP9UfZhlubg'
     });
-    // this.dataSources.forEach((source, index) => {
-    //   this.map?.addSource(`source-${index}`, source);
-    // });
+
     this.map.on('load', () => {
-      this.map.addSource('national-park', this.parkSource);
+      this.dataSources.forEach((source, index) => {
+        this.map?.addSource(`source-${index}`, source);
+      });
+
+      this.map.addControl(new mapboxgl.NavigationControl, 'top-right');
     });
   }
 }
