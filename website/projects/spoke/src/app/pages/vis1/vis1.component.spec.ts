cd568f741ea96e1ac736b2b8c750c5897dae400e
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-04-09 16:44:59 -0400

    Change testing

--- website/projects/spoke/src/app/pages/vis1/vis1.component.spec.ts
+++ website/projects/spoke/src/app/pages/vis1/vis1.component.spec.ts
@@ -11,6 +11,34 @@
 //   });
 
 //   it('creates the vega spec', async () => {
-//     expect().nothing();
+//     const { instance } = await shallow.render();
+//     const spy = spyOn(instance, 'createSpec');
+//     expect(spy).toHaveBeenCalled();
 //   });
 // });
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Vis1Component } from './vis1.component';
+
+describe('ScatterplotComponent', () => {
+  let component: Vis1Component;
+  let fixture: ComponentFixture<Vis1Component>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Vis1Component ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(Vis1Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
