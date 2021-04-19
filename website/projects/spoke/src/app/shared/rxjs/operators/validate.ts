b76fcfec54786f3889538fb2287d7394e6869f60
axdanbol <daniel_bolin@yahoo.com>
2021-04-19 12:27:11 -0400

    Added rxjs utility operator

---
+++ website/projects/spoke/src/app/shared/rxjs/operators/validate.ts
@@ -0,0 +1,27 @@
+import { MonoTypeOperatorFunction, OperatorFunction } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+
+export function validate<T, U extends T>(
+  pred: (data: T) => data is U,
+  message?: string,
+  errorType?: new (message?: string) => unknown
+): OperatorFunction<T, U>;
+
+export function validate<T>(
+  pred: (data: T) => boolean,
+  message?: string,
+  errorType?: new (message?: string) => unknown
+): MonoTypeOperatorFunction<T>;
+
+export function validate(
+  pred: (data: unknown) => boolean,
+  message?: string,
+  errorType?: new (message?: string) => unknown
+): MonoTypeOperatorFunction<unknown> {
+  return tap(data => {
+    if (!pred(data)) {
+      throw new (errorType ?? Error)(message);
+    }
+  });
+}
