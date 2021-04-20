663531c341c432f071d6f50de46cca7f705831df
axdanbol <daniel_bolin@yahoo.com>
2021-04-20 01:46:52 -0400

    Implemented footer (missing theming)

--- website/projects/spoke/src/app/core/components/footer/footer.component.spec.ts
+++ website/projects/spoke/src/app/core/components/footer/footer.component.spec.ts
@@ -1,7 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Shallow } from 'shallow-render';
 
 import { FooterComponent } from './footer.component';
+import { FooterModule } from './footer.module';
 
 
 describe('FooterComponent', () => {
+  let shallow: Shallow<FooterComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(FooterComponent, FooterModule);
+  });
+
+  it('creates', async () => {
+    const { instance } = await shallow.render();
+    expect(instance).toBeDefined();
+  });
 });
--- website/projects/spoke/src/app/core/components/footer/footer.component.ts
+++ website/projects/spoke/src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 
 
 @Component({
@@ -7,4 +7,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FooterComponent { }
+export class FooterComponent {
+  @Output() termsOfUseClicked = new EventEmitter<void>();
+}
