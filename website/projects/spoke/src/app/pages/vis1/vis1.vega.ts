84efd43e090b5ac07eb677595d073116cfae9b40
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-04-20 03:24:32 -0400

    Add disease and food selection

--- website/projects/spoke/src/app/pages/vis1/vis1.vega.ts
+++ website/projects/spoke/src/app/pages/vis1/vis1.vega.ts
@@ -1,5 +1,478 @@
 import { VisualizationSpec } from 'vega-embed';
 
+export interface SpecOptions {
+  overviewNodes: any[];
+  overviewEdges: any[];
+  diseaseEdges?: any[];
+  targetFood?: string;
+}
+
+export function createOverviewSpec() {
+  return vis1Spec;
+}
+
+export function createSpec(source?: string, destination?: string, options?: SpecOptions): VisualizationSpec {
+  return {
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    config: {
+      view: {
+        stroke: 'transparent'
+      }
+    },
+    width: 1500,
+    height: 750,
+    layer: [
+      {
+        data: {
+          url: 'assets/datasets/overview/edges-v3.csv'
+        },
+        transform: [{
+          joinaggregate: [{
+            op: 'sum',
+            field: 'weight',
+            as: 'total_weight'
+          }],
+          groupby: ['source_type', 'target_type']
+        }],
+        mark: {
+          type: 'rule'
+        },
+        params: [
+          {
+            name: 'highlightEdge',
+            select: {type: 'point', on: 'mouseover'}
+          }
+        ],
+        encoding: {
+          x: {
+            field: 'a1',
+            type: 'quantitative'
+          },
+          y: {
+            field: 'b1',
+            type: 'quantitative'
+          },
+          x2: {
+            field: 'a2'
+          },
+          y2: {
+            field: 'b2'
+          },
+          strokeWidth: {
+            field: 'total_weight',
+            scale: {
+              range: [1, 10]
+            },
+            legend: null
+          },
+          strokeOpacity: {
+            value: 0.15
+          },
+          color: {
+            condition: [
+              {
+                param: 'highlightEdge',
+                empty: false,
+                value: 'red'
+              }
+            ],
+            field: 'total_weight',
+            scale: {
+              scheme: "greys"
+            },
+            legend: null
+          },
+          tooltip: [
+            {
+              field: 'tooltip',
+              title: 'Link Types (#Links)',
+              type: 'ordinal'
+            }
+          ]
+        }
+      },
+      {
+        data: {
+          url:  `assets/datasets/${source}-food-tree/tree-edges.csv`
+        },
+        transform: [
+          {
+            lookup: 'source_type',
+            from: {
+              data: {url: 'assets/datasets/overview/nodes-v3.csv'},
+              key: 'type',
+              fields: ['a', 'b']
+            },
+            as: ['a1', 'b1']
+          },
+          {
+            lookup: 'target_type',
+            from: {
+              data: {url: 'assets/datasets/overview/nodes-v3.csv'},
+              key: 'type',
+              fields: ['a', 'b']
+            },
+            as: ['a2', 'b2']
+          },
+          {filter: `datum.dest_name == '${destination}'`}
+        ],
+        mark: {
+          type: 'rule'
+        },
+        encoding: {
+          x: {
+            field: 'a1',
+            type: 'quantitative'
+          },
+          y: {
+            field: 'b1',
+            type: 'quantitative'
+          },
+          x2: {
+            field: 'a2'
+          },
+          y2: {
+            field: 'b2'
+          }
+        }
+      },
+      {
+        data: {
+          url: 'assets/datasets/overview/nodes-v3.csv'
+        },
+        mark: {
+          type: 'circle',
+          stroke: 'red',
+          strokeWidth: 2
+        },
+        params: [
+          {
+            name: 'highlightNode',
+            select: {type: 'point', on: 'mouseover'}
+          }
+        ],
+        encoding: {
+          strokeOpacity: {
+            value: 0,
+            condition: [
+              {
+                param: 'highlightNode',
+                empty: false,
+                value: 100
+              }
+            ]
+          },
+          opacity: {
+            condition: {
+              test: "datum.label === 'Disease'", 
+              value: 1
+            },
+            value: 0.15
+          },
+          x: {
+            field: 'a',
+            type: 'quantitative',
+            axis: {
+              tickCount: 12,
+              domain: false,
+              labels: false,
+              ticks: false,
+              title: null
+            },
+            scale: {
+              domain: [0, 11]
+            }
+          },
+          y: {
+            field: 'b',
+            type: 'quantitative',
+            axis: {
+              tickCount: 5,
+              domain: false,
+              labels: false,
+              ticks: false,
+              title: null,
+              gridOpacity: 0.5
+            },
+            scale: {
+              domain: [0, 6]
+            }
+          },
+          tooltip: [
+            {
+              field: 'tooltip',
+              title: 'Link Types (#Links)',
+              type: 'ordinal'
+            }
+          ],
+          color: {
+            field: 'id',
+            type: 'quantitative',
+            legend: null,
+            scale: {
+              scheme: 'category20'
+            }
+          },
+          size: {
+            field: 'weight',
+            type: 'quantitative',
+            scale: {
+              range: [500, 1000]
+            },
+            legend: null
+          }
+        }
+      },
+      {
+        data: {
+          url:  `assets/datasets/${source}-food-tree/tree-edges.csv`
+        },
+        transform: [
+          {
+            lookup: 'target_type',
+            from: {
+              data: {url: 'assets/datasets/overview/nodes-v3.csv'},
+              key: 'type',
+              fields: ['a', 'b', 'id', 'weight']
+            }
+          },
+          {filter: `datum.dest_name == '${destination}'`}
+        ],
+        mark: {
+          type: 'circle',
+          opacity: 1,
+          stroke: 'red',
+          strokeWidth: 2
+        },
+        encoding: {
+          strokeOpacity: {
+            value: 0
+          },
+          x: {
+            field: 'a',
+            type: 'quantitative',
+            axis: {
+              tickCount: 12,
+              domain: false,
+              labels: false,
+              ticks: false,
+              title: null
+            },
+            scale: {
+              domain: [0, 11]
+            }
+          },
+          y: {
+            field: 'b',
+            type: 'quantitative',
+            axis: {
+              tickCount: 5,
+              domain: false,
+              labels: false,
+              ticks: false,
+              title: null,
+              gridOpacity: 0.5
+            },
+            scale: {
+              domain: [0, 6]
+            }
+          },
+          color: {
+            field: 'id',
+            type: 'quantitative',
+            legend: null,
+            scale: {
+              scheme: 'category20'
+            }
+          },
+          size: {
+            field: 'weight',
+            type: 'quantitative',
+            scale: {
+              range: [500, 1000]
+            },
+            legend: null
+          }
+        }
+      },
+      {
+        data: {
+          url: 'assets/datasets/overview/nodes-v3.csv'
+        },
+        mark: {
+          type: 'text', 
+          dy: -30, 
+          fontSize: 12
+        },
+        encoding: {
+          text: {
+            field: 'label', 
+            type: 'nominal'
+          },
+          x: {
+            field: 'a',
+            type: 'quantitative'
+          },
+          y: {
+            field: 'b',
+            type: 'quantitative'
+          },
+          opacity: {
+            condition: {
+              test: "datum.label === 'Disease'", 
+              value: 1
+            },
+            value: 0.15
+          }
+        }
+      },
+      {
+        data: {
+          url:  `assets/datasets/${source}-food-tree/tree-edges.csv`
+        },
+        transform: [
+          {
+            lookup: 'target_type',
+            from: {
+              data: {url: 'assets/datasets/overview/nodes-v3.csv'},
+              key: 'type',
+              fields: ['a', 'b', 'label']
+            }
+          },
+          {filter: `datum.dest_name == '${destination}'`}
+        ],
+        mark: {
+          type: 'text', 
+          dy: -30, 
+          fontSize: 12,
+          opacity: 1
+        },
+        encoding: {
+          text: {
+            field: 'label', 
+            type: 'nominal'
+          },
+          x: {
+            field: 'a',
+            type: 'quantitative'
+          },
+          y: {
+            field: 'b',
+            type: 'quantitative'
+          }
+        }
+      },
+      {
+        data: {
+          values: [
+            {x: 11, y: 5, label: 'B'},
+            {x: 11, y: 4, label: 'C'},
+            {x: 11, y: 3, label: 'D'},
+            {x: 11, y: 2, label: 'E'},
+            {x: 11, y: 1, label: 'F'}
+          ]
+        },
+        mark: {
+          type: 'text', 
+          fontSize: 12,
+          dx: 10,
+          opacity: 0.5
+        },
+        encoding: {
+          text: {
+            field: 'label', 
+            type: 'nominal'
+          },
+          x: {
+            field: 'x',
+            type: 'quantitative'
+          },
+          y: {
+            field: 'y',
+            type: 'quantitative'
+          }
+        }
+      },
+      {
+        data: {
+          values: [
+            {x: 0, y: 0, label: '1'},
+            {x: 1, y: 0, label: '2'},
+            {x: 2, y: 0, label: '3'},
+            {x: 3, y: 0, label: '4'},
+            {x: 4, y: 0, label: '5'},
+            {x: 5, y: 0, label: '6'},
+            {x: 6, y: 0, label: '7'},
+            {x: 7, y: 0, label: '8'},
+            {x: 8, y: 0, label: '9'},
+            {x: 9, y: 0, label: '10'},
+            {x: 10, y: 0, label: '11'},
+            {x: 11, y: 0, label: '12:G'}
+          ]
+        },
+        mark: {
+          type: 'text', 
+          fontSize: 12,
+          dy: 10,
+          opacity: 0.5
+        },
+        encoding: {
+          text: {
+            field: 'label', 
+            type: 'nominal'
+          },
+          x: {
+            field: 'x',
+            type: 'quantitative'
+          },
+          y: {
+            field: 'y',
+            type: 'quantitative'
+          }
+        }
+      },
+      {
+        data: {
+          values: [
+            {x: 0, y: 6, label: '1'},
+            {x: 1, y: 6, label: '2'},
+            {x: 2, y: 6, label: '3'},
+            {x: 3, y: 6, label: '4'},
+            {x: 4, y: 6, label: '5'},
+            {x: 5, y: 6, label: '6'},
+            {x: 6, y: 6, label: '7'},
+            {x: 7, y: 6, label: '8'},
+            {x: 8, y: 6, label: '9'},
+            {x: 9, y: 6, label: '10'},
+            {x: 10, y: 6, label: '11'},
+            {x: 11, y: 6, label: '12:A'}
+          ]
+        },
+        mark: {
+          type: 'text', 
+          fontSize: 12,
+          dy: -10,
+          opacity: 0.5
+        },
+        encoding: {
+          text: {
+            field: 'label', 
+            type: 'nominal'
+          },
+          x: {
+            field: 'x',
+            type: 'quantitative'
+          },
+          y: {
+            field: 'y',
+            type: 'quantitative'
+          }
+        }
+      }
+    ]
+  };
+}
+
 export const vis1Spec: VisualizationSpec =
   {
     $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
@@ -64,7 +537,12 @@ export const vis1Spec: VisualizationSpec =
                 empty: false,
                 value: 'red'
               }
-            ]
+            ],
+            field: 'total_weight',
+            scale: {
+              scheme: 'greys'
+            },
+            legend: null
           },
           tooltip: [
             {
@@ -136,8 +614,12 @@ export const vis1Spec: VisualizationSpec =
             }
           ],
           color: {
-            field: 'label',
-            legend: null
+            field: 'id',
+            type: 'quantitative',
+            legend: null,
+            scale: {
+              scheme: 'category20'
+            }
           },
           size: {
             field: 'weight',
@@ -279,3 +761,4 @@ export const vis1Spec: VisualizationSpec =
       }
     ]
   };
+  
