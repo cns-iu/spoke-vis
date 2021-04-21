74252c96a83094027bb417389f289374f69cb79a
Bruce Herr II <bherr@indiana.edu>
2021-04-21 13:30:57 -0400

    First pass on getting markers on the map based on the disease and food

--- website/projects/spoke/src/app/features/visualizations/pages/detail/detail.component.ts
+++ website/projects/spoke/src/app/features/visualizations/pages/detail/detail.component.ts
@@ -1,9 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FeatureCollection } from 'geojson';
+import { FeatureCollection, Point } from 'geojson';
 import { forkJoin, Subscription } from 'rxjs';
 import { filter, map, switchMap } from 'rxjs/operators';
+
+import { MapMarker } from './../../../../core/models/Map';
+
 
 const EMPTY_FEATURES: FeatureCollection = {
   type: 'FeatureCollection', features: []
@@ -23,6 +26,8 @@ export class DetailComponent implements OnInit, OnDestroy {
   nodeFeatures = EMPTY_FEATURES;
   clusterFeatures = EMPTY_FEATURES;
   boundaryFeatures = EMPTY_FEATURES;
+  markers: MapMarker[] = [];
+  mapCenter: [number, number] = [0, -40];
 
   readonly mapData$ = this.route.paramMap.pipe(
     map(p => {
@@ -32,12 +37,36 @@ export class DetailComponent implements OnInit, OnDestroy {
     }),
     filter(({disease, food}) => disease !== undefined),
     switchMap(({disease, food}) => {
+      // Replace this with a lookup table in appropriate state (or in the URL)
+      const diseaseLabel = ({
+        cad: 'coronary artery disease',
+        alzheimers: 'Alzheimer\'s disease',
+        'alzheimers-all': 'Alzheimer\'s disease',
+        epilepsy: 'epilepsy'
+      } as {[dataset: string]: string})[disease as string] || disease;
+
       const dataDir = `assets/datasets/${disease}-food-tree`;
+      const nodes = this.http.get<FeatureCollection<Point, {label: string}>>(`${dataDir}/nodes.geojson`);
+      const markers = nodes.pipe(map(n => {
+        return n.features.filter(node => node.properties.label === food || node.properties.label === diseaseLabel)
+          .map(node => ({
+            config: {
+              color: node.properties.label === food ? '#D1B445' : '#880E4F'
+            },
+            coordinates: node.geometry.coordinates
+          }) as MapMarker)
+      }));
+      const mapCenter = nodes.pipe(map(n =>
+        n.features.find(node => node.properties.label === diseaseLabel)?.geometry.coordinates as [number, number] ?? [0, -40]
+      ));
+
       return forkJoin({
         edges: this.http.get<FeatureCollection>(`${dataDir}/edges.geojson`),
-        nodes: this.http.get<FeatureCollection>(`${dataDir}/nodes.geojson`),
+        nodes,
         clusters: this.http.get<FeatureCollection>(`${dataDir}/cluster.geojson`),
-        boundaries: this.http.get<FeatureCollection>(`${dataDir}/boundary.geojson`)
+        boundaries: this.http.get<FeatureCollection>(`${dataDir}/boundary.geojson`),
+        markers,
+        mapCenter
       });
     })
   );
@@ -49,11 +78,13 @@ export class DetailComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.subscriptions.add(
       this.mapData$.subscribe({
-        next: ({edges, nodes, clusters, boundaries}) => {
+        next: ({edges, nodes, clusters, boundaries, markers, mapCenter}) => {
           this.edgeFeatures = edges;
           this.nodeFeatures = nodes;
           this.clusterFeatures = clusters;
           this.boundaryFeatures = boundaries;
+          this.markers = markers;
+          this.mapCenter = mapCenter;
           this.cd.detectChanges();
         },
         error: () => {
@@ -61,6 +92,8 @@ export class DetailComponent implements OnInit, OnDestroy {
           this.nodeFeatures = EMPTY_FEATURES;
           this.clusterFeatures = EMPTY_FEATURES;
           this.boundaryFeatures = EMPTY_FEATURES;
+          this.markers = [];
+          this.mapCenter = [0, -40];
           this.cd.detectChanges();
         }
       })
