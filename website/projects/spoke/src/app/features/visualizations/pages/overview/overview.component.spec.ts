281a9bba50fc7dcfc1d5d7d9b9bb9f7226ee073a
axdanbol <daniel_bolin@yahoo.com>
2021-04-22 00:59:36 -0400

    Tweaked and tested overview component

--- website/projects/spoke/src/app/features/visualizations/pages/overview/overview.component.spec.ts
+++ website/projects/spoke/src/app/features/visualizations/pages/overview/overview.component.spec.ts
@@ -1,3 +1,34 @@
+import { RouterStateSnapshot } from '@angular/router';
+import { Subject } from 'rxjs';
+import { RecursivePartial, Shallow } from 'shallow-render';
+
+import { RouterState } from '../../../../core/state/router.state';
+import { OverviewComponent } from './overview.component';
+import { OverviewModule } from './overview.module';
+
 
 describe('OverviewComponent', () => {
+  let routerState: Subject<RecursivePartial<RouterStateSnapshot>>;
+  let shallow: Shallow<OverviewComponent>;
+
+  beforeEach(() => {
+    routerState = new Subject();
+    shallow = new Shallow(OverviewComponent, OverviewModule)
+      .mock(RouterState, {
+        state$: routerState as Subject<RouterStateSnapshot>
+      });
+  });
+
+  describe('spec$', () => {
+    it('creates specs with the current disease and food', async () => {
+      const disease = 'flu';
+      const food = 'pasta';
+      const { instance } = await shallow.render();
+
+      spyOn(instance, 'createSpec');
+      routerState.next({ root: { queryParams: { disease, food } } });
+
+      expect(instance.createSpec).toHaveBeenCalledWith({ source: disease, destination: food });
+    });
+  });
 });
--- website/projects/spoke/src/app/features/visualizations/pages/overview/overview.component.ts
+++ website/projects/spoke/src/app/features/visualizations/pages/overview/overview.component.ts
@@ -2,13 +2,16 @@ import { Immutable } from '@angular-ru/common/typings';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, map, pluck } from 'rxjs/operators';
+import { filter, map, pluck, startWith } from 'rxjs/operators';
 import { VisualizationSpec } from 'vega-embed';
 
 import { RouterState } from '../../../../core/state/router.state';
-import { createSpec } from './overview-visualization.vega';
+import { createSpec, SpecOptions } from './overview-visualization.vega';
 
 
+/**
+ * Displays an overview visualization
+ */
 @Component({
   selector: 'spoke-overview',
   templateUrl: './overview.component.html',
@@ -16,13 +19,32 @@ import { createSpec } from './overview-visualization.vega';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OverviewComponent {
+  /** The latest visualization spec */
   readonly spec$: Observable<VisualizationSpec>;
 
+  /**
+   * Creates an instance of overview component.
+   *
+   * @param router Router state used to lookup query parameters
+   */
   constructor(router: RouterState) {
     this.spec$ = router.state$.pipe(
       filter((value): value is Immutable<RouterStateSnapshot> => !!value),
       pluck('root', 'queryParams'),
-      map(({ disease, food }) => createSpec({ source: disease, destination: food }))
+      map(({ disease, food }) => this.createSpec({ source: disease, destination: food })),
+      startWith({})
     );
+  }
+
+  /**
+   * Creates a spec with the provided options
+   * This method mainly exists to make mocking possible in tests
+   *
+   * @param options The spec options
+   * @returns The created spec
+   */
+  /* istanbul ignore next */
+  createSpec(options: SpecOptions): VisualizationSpec {
+    return createSpec(options);
   }
 }
