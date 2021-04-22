bf07a19fdbcb5517930040b972728c3b093c9cc6
axdanbol <daniel_bolin@yahoo.com>
2021-04-22 00:18:22 -0400

    Switched to using query params for disease and food

--- website/projects/spoke/src/app/features/visualizations/pages/overview/overview.component.ts
+++ website/projects/spoke/src/app/features/visualizations/pages/overview/overview.component.ts
@@ -1,9 +1,11 @@
-import { Any } from '@angular-ru/common/typings';
+import { Immutable } from '@angular-ru/common/typings';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map, pluck } from 'rxjs/operators';
+import { VisualizationSpec } from 'vega-embed';
 
+import { RouterState } from '../../../../core/state/router.state';
 import { createSpec } from './overview-visualization.vega';
 
 
@@ -14,13 +16,13 @@ import { createSpec } from './overview-visualization.vega';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class OverviewComponent {
-  // Any is required here because Angular thinks this sometimes returns null when it doesn't
-  readonly spec$: Observable<Any> = this.route.paramMap.pipe(map(p =>
-    createSpec({
-      source: p.get('disease') || undefined,
-      destination: p.get('food') || undefined
-    }))
-  );
+  readonly spec$: Observable<VisualizationSpec>;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(router: RouterState) {
+    this.spec$ = router.state$.pipe(
+      filter((value): value is Immutable<RouterStateSnapshot> => !!value),
+      pluck('root', 'queryParams'),
+      map(({ disease, food }) => createSpec({ source: disease, destination: food }))
+    );
+  }
 }
