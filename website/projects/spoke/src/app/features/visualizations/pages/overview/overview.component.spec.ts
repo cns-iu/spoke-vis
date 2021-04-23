16f05942f85f82a772546c73de35648b298bb1b1
axdanbol <daniel_bolin@yahoo.com>
2021-04-23 15:48:03 -0400

    Fixed code smell

--- website/projects/spoke/src/app/features/visualizations/pages/overview/overview.component.spec.ts
+++ website/projects/spoke/src/app/features/visualizations/pages/overview/overview.component.spec.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 import { RecursivePartial, Shallow } from 'shallow-render';
 
 import { RouterState } from '../../../../core/state/router.state';
+import { DatasetState } from '../../state/dataset.state';
 import { OverviewComponent } from './overview.component';
 import { OverviewModule } from './overview.module';
 
@@ -16,6 +17,12 @@ describe('OverviewComponent', () => {
     shallow = new Shallow(OverviewComponent, OverviewModule)
       .mock(RouterState, {
         state$: routerState as Subject<RouterStateSnapshot>
-      });
+      })
+      .mock(DatasetState, { });
+  });
+
+  it('creates', async () => {
+    const { instance } = await shallow.render();
+    expect(instance).toBeDefined();
   });
 });
