17f4faed904018ad0630f5f18c72fdae2f0b15ff
Bruce W. Herr II <bherr@indiana.edu>
2021-04-22 21:08:21 -0400

    log more things (#34)
    * log more things
    
    * log more of the things

--- website/projects/spoke/src/app/core/components/header/header.component.ts
+++ website/projects/spoke/src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GoogleAnalyticsService } from 'ngx-google-analytics';
 
 
 @Component({
@@ -9,4 +10,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 })
 export class HeaderComponent {
   menuOpen = false;
+
+  constructor(private ga: GoogleAnalyticsService) { }
+
+  logClick(siteName: string): void {
+    this.ga.event('header_view', 'link_click', siteName);
+  }
+
+  setMenuOpen(value: boolean): void {
+    this.menuOpen = value;
+    const label = value ? 'menu_opened' : 'menu_closed';
+    this.ga.event('header_view', 'menu_toggled', label);
+  }
 }
