3b2265be23f3855dbec9f80b26e4734cadfccda7
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-06-22 15:41:10 -0400

    Pbi 41 user tracking notification (#45)
    * Add snackbar component to let user opt in/out of tracking
    
    * Make custom google analytics service
    
    * Fix linting and other issues
    
    * Fix linting
    
    * Remove default test
    
    * Fix bugs and code smells; add ngxsOnInit
    
    * Change true to false
    
    * Linting
    
    * Change popup behavior
    
    * Update popup behavior
    
    * Remove console.log
    
    * Refactoring
    
    * Gtag token and page state changes
    
    * Properly disable google analytics when user opts out
    
    * Delete settings.json; fix linting
    
    * Fix linting
    
    * Code smell

---
+++ website/projects/spoke/src/app/core/components/tracking-popup/tracking-popup.component.spec.ts
@@ -0,0 +1,24 @@
+import { Shallow } from 'shallow-render';
+import { ElementRef } from '@angular/core';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+import { PageState } from '../../state/page.state';
+
+import { TrackingPopupComponent } from './tracking-popup.component';
+import { TrackingPopupModule } from './tracking-popup.module';
+
+
+describe('TrackingPopupComponent', () => {
+  let shallow: Shallow<TrackingPopupComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(TrackingPopupComponent, TrackingPopupModule)
+      .provide({ provide: ElementRef, useValue: {} })
+      .provide({ provide: MAT_SNACK_BAR_DATA, useValue: {} })
+      .provide({ provide: PageState, useValue: {} });
+  });
+
+  it('creates', async () => {
+    const { instance } = await shallow.render();
+    expect(instance).toBeDefined();
+  });
+});
